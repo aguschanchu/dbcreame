5400a8ed4c211ec2dcc7a3e0466804c3b5dacec2
Agustin Cianciulli <agusc@agusc.com.ar>
2018-07-29 22:58:28 -0300

    Category and object view

--- db/urls.py
+++ db/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 from db import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^category/(?P<category>[\w.@+-]+)/$', views.CategoryView.as_view()),
-    url(r'^object/(?P<id>[\w.@+-]+)/$', views.ObjectView.as_view()),
+    path('view/<uuid:id>/', views.ObjectView.as_view()),
 ]
--- db/views.py
+++ db/views.py
@@ -1,7 +1,11 @@
 from db.serializers import ObjetoSerializer
 from .models import Objeto
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
+from django.http import Http404
+import json
 
 class CategoryView(generics.ListAPIView):
     serializer_class = ObjetoSerializer
@@ -12,11 +16,16 @@ class CategoryView(generics.ListAPIView):
         objetos = Objeto.objects.filter(category__name=category)
         return objetos
 
-class ObjectView(generics.RetrieveAPIView):
-    serializer_class = ObjetoSerializer
-    lookup_url_kwarg = 'id'
-
-    def get_queryset(self):
-        id = self.kwargs.get(self.lookup_url_kwarg)
-        objeto = Objeto.objects.get(id=id)
-        return objeto
+class ObjectView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            objeto = Objeto.objects.get(pk=id)
+        except:
+            raise Http404
+        serializer = ObjetoSerializer(objeto)
+        respuesta = serializer.data
+        #Reemplazamos las ids de categorias, tags, y autores, por sus respectivos nombres
+        respuesta['category'] = json.dumps([a.name for a in objeto.category.all()])
+        respuesta['tags'] = json.dumps([a.name for a in objeto.tags.all()])
+        respuesta['author'] = objeto.author.name
+        return Response(respuesta)
