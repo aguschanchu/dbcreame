ba9927d907396bbc5a8ebd8078da5272fa425445
Agustin Cianciulli <agusc@agusc.com.ar>
2018-09-08 16:49:21 -0300

    Just giving Pycharm a try

--- migrate.sh
+++ migrate.sh
@@ -2,4 +2,14 @@ rm -r db/migrations media tmp
 mkdir media tmp
 mkdir media/images media/renders media/sfb media/stl
 mkdir media/images/plots
+#PSQL DB preparation
+sudo -u postgres -H -- psql -c "DROP DATABASE dbapi"
+sudo -u postgres -H -- psql -c "CREATE USER dbapi WITH PASSWORD '***REMOVED***'"
+sudo -u postgres -H -- psql -c "DROP DATABASE dbapi"
+sudo -u postgres -H -- psql -c "CREATE DATABASE dbapi"
+sudo -u postgres -H -- psql -c "ALTER ROLE dbapi SET client_encoding TO 'utf8'"
+sudo -u postgres -H -- psql -c "ALTER ROLE dbapi SET default_transaction_isolation TO 'read committed'"
+sudo -u postgres -H -- psql -c "ALTER ROLE dbapi SET timezone TO 'UTC'"
+sudo -u postgres -H -- psql -c "GRANT ALL PRIVILEGES ON DATABASE dbapi TO dbapi"
+#DB population
 python manage.py makemigrations db && python manage.py migrate && python populate.py
