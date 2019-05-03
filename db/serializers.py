d8713a59daf1f373f2a5c3a2783c631b527c9cab
Agustin Cianciulli <agusc@agusc.con.ar>
2019-05-03 19:17:18 -0300

    Update user info on new order

--- db/serializers.py
+++ db/serializers.py
@@ -101,12 +101,6 @@ class CategoriaSerializer(serializers.ModelSerializer):
         model = Categoria
         fields = '__all__'
 
-
-'''
-Definimos el serializador de Compra con un poco mas de cuidado, para poder serializar
-en forma nesteada una compra; simplificando la creacion de estas.
-'''
-
 class ObjetoPersonalizadoSerializer(serializers.ModelSerializer):
     thumbnail = serializers.ImageField(allow_empty_file=True,read_only=True,use_url=True)
 
@@ -130,35 +124,6 @@ class DireccionDeEnvioSerializer(serializers.ModelSerializer):
         model = DireccionDeEnvio
         fields = ('short_address','notes','last_time_used','long_address','gmaps_id')
 
-class CompraSerializer(serializers.ModelSerializer):
-    purchased_objects = ObjetoPersonalizadoSerializer(many=True)
-    payment_preferences = PaymentPreferencesSerializer(required=False, allow_null=True)
-    delivery_address = DireccionDeEnvioSerializer()
-
-    class Meta:
-        model = Compra
-        fields = ('id','buyer','purchased_objects','date','status','payment_preferences','delivery_address')
-
-    #DRF no soporta creacion de objetos nesteados out-of-the-box, de modo, que reemplazamos el metodo de creacion
-    def create(self, validated_data):
-        #Obtenemos DireccionDeEnvio a partir de delivery_address_char
-        delivery_address_data = validated_data.pop('delivery_address')
-        delivery_address, created = DireccionDeEnvio.objects.get_or_create(gmaps_id=delivery_address_data['gmaps_id'],  usuario=self.context['request'].user.usuario)
-        notes = delivery_address_data['notes'] if 'notes' in delivery_address_data else None
-        if created:
-            delivery_address.notes = notes
-        ## Actualizamos la ultima vez que se uso esta direccion
-        else:
-            delivery_address.last_time_used = datetime.datetime.now()
-            if notes != None:
-                delivery_address.notes = notes
-        delivery_address.save()
-        #Cargamos los objetos comprados
-        purchased_objects_data = validated_data.pop('purchased_objects')
-        compra = Compra.objects.create(delivery_address=delivery_address, **validated_data)
-        for purchased_object_data in purchased_objects_data:
-            ObjetoPersonalizado.objects.create(purchase=compra, **purchased_object_data)
-        return compra
 
 '''
 Serializadores accesorios
@@ -181,19 +146,20 @@ class UsuarioSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(source='user.last_name', required=False)
     email = serializers.CharField(source='user.email', required=False)
     date_joined = serializers.DateTimeField(source='user.username', required=False)
-    address_book = DireccionDeEnvioSerializer(many=True,required=False)
+    address_book = DireccionDeEnvioSerializer(many=True, required=False)
 
     class Meta:
         model = Usuario
         depth = 1
         fields = ('id','username','first_name','last_name','email','date_joined','address_book','telephone')
 
-    def update(self, instance, validated_data):
-        user = self.context['request'].user
+    def update(self, instance, validated_data, user = None):
+        if user is None:
+            user = self.context['request'].user
         #Actualizamos el User
         for key, value in validated_data['user'].items():
             #Unicamente permitimos que cambien ciertos atributos de usuario
-            if key in ['first_name', 'last_name', 'email']:
+            if key in ['first_name', 'last_name', 'email', 'telephone'] and value is not None:
                 setattr(user, key, value)
         #Actualizamos el Usuario
         if 'telephone' in validated_data.keys():
@@ -206,3 +172,54 @@ class AppSetupInformationSerializer(serializers.Serializer):
     discount_parameter_a = serializers.FloatField(read_only=True)
     discount_parameter_b = serializers.FloatField(read_only=True)
 
+'''
+Definimos el serializador de Compra con un poco mas de cuidado, para poder serializar
+en forma nesteada una compra; simplificando la creacion de estas.
+'''
+
+
+class CompraSerializer(serializers.ModelSerializer):
+    purchased_objects = ObjetoPersonalizadoSerializer(many=True)
+    payment_preferences = PaymentPreferencesSerializer(required=False, allow_null=True)
+    delivery_address = DireccionDeEnvioSerializer()
+    buyer = UsuarioSerializer(required=False, allow_null=True)
+
+    class Meta:
+        model = Compra
+        fields = ('id','buyer','purchased_objects','date','status','payment_preferences','delivery_address')
+
+    #DRF no soporta creacion de objetos nesteados out-of-the-box, de modo, que reemplazamos el metodo de creacion
+    def create(self, validated_data):
+        #Actualiamos los datos del comprador
+        user = self.context['request'].user
+        ## Actualizamos el User
+        if 'buyer' in validated_data.keys():
+            buyer_info = validated_data.pop('buyer')
+            for key, value in buyer_info['user'].items():
+                #Unicamente permitimos que cambien ciertos atributos de usuario
+                if key in ['first_name', 'last_name', 'email'] and value is not None:
+                    setattr(user, key, value)
+            ## Actualizamos el Usuario
+            telephone = buyer_info['telephone'] if 'telephone' in buyer_info.keys() else None
+            if telephone is not None:
+                user.usuario.telephone = telephone
+                user.usuario.save()
+            user.save()
+        #Obtenemos DireccionDeEnvio a partir de delivery_address_char
+        delivery_address_data = validated_data.pop('delivery_address')
+        delivery_address, created = DireccionDeEnvio.objects.get_or_create(gmaps_id=delivery_address_data['gmaps_id'],  usuario=self.context['request'].user.usuario)
+        notes = delivery_address_data['notes'] if 'notes' in delivery_address_data else None
+        if created:
+            delivery_address.notes = notes
+        ## Actualizamos la ultima vez que se uso esta direccion
+        else:
+            delivery_address.last_time_used = datetime.datetime.now()
+            if notes != None:
+                delivery_address.notes = notes
+        delivery_address.save()
+        #Cargamos los objetos comprados
+        purchased_objects_data = validated_data.pop('purchased_objects')
+        compra = Compra.objects.create(delivery_address=delivery_address, **validated_data)
+        for purchased_object_data in purchased_objects_data:
+            ObjetoPersonalizado.objects.create(purchase=compra, **purchased_object_data)
+        return compra
